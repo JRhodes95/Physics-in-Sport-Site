a5acbaf0199b21f3ed83a4cb666154569873b2b8
Jack Rhodes <jack-rhodes@hotmail.co.uk>
2017-02-18 18:44:47 +0000

    Added online quiz generator fucntionality

--- quiz.js
+++ quiz.js
@@ -1,8 +1,9 @@
-var linebreak = document.createElement('br');
+
 
 
 function getQuiz(topic_name){
-    
+    //get the correct quiz from the server using get request
+    var linebreak = document.createElement('br');
     
     $.ajax({
         url: "quiz.php",
@@ -11,39 +12,55 @@ function getQuiz(topic_name){
             topic: topic_name, 
         },
         success: function(response) {
-            console.log(response);
             
+            //log the response to the server
+            console.log(response);
+            //get the div that will hold all the quiz stuff
             var quizDiv = document.getElementById("fillQuiz");
             
+            //empty the div of any content
+            $('#fillQuiz').empty();
+           
+            //Get the JSON for the topic
             var questions = response[topic_name];
+            console.log(questions);
+            var topic_form= document.createElement('form');
             
             for (i=0; i<questions.length; i++){
+                //for each question object 
                 
+                //Extract the question
                 var question_i = questions[i]["question"]
                 
-                //make the question entry
+                console.log(question_i);
+                //Make it a heading on the page
                 var question_text = document.createElement('h4');
                 question_text.appendChild(document.createTextNode(question_i));
+                
+                //create the quiz form
+                
+                
                 quizDiv.appendChild(question_text);
                 
-                var topic_form= document.createElement('form');
-                quizDiv.appendChild(topic_form);
-                
                 for (j=0; j<3; j++) {
+                    //for each option, make a radio button on the form 
                     
                     var option_i = questions[i]["option "+(j+1)];
-
                     var option_input = document.createElement('input');
                     option_input.setAttribute('type','radio');
-                    option_input.appendChild(document.createTextNode(option_i));
-                    option_input.appendChild(linebreak);
+                    option_input.setAttribute('name','q1');
+                    option_input.setAttribute('value','a'+(j+1));
                     topic_form.appendChild(option_input);
+                    topic_form.appendChild(document.createTextNode(option_i));
+                    topic_form.appendChild(linebreak);
+                    
                     
                 }
                 
                 
+                
             }
-            
+            quizDiv.appendChild(topic_form);
             
                 
             
