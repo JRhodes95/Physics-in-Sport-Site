60ec2ed47ac61a25765a37a79114ee8be3f6c5b4
Jack Rhodes <jack-rhodes@hotmail.co.uk>
2017-02-22 15:27:47 +0000

    PRe navbar edit

--- tablinks.js
+++ tablinks.js
@@ -1,11 +1,14 @@
 // Javascript to enable link to tab
-var hash = document.location.hash;
-var prefix = "tab_";
-if (hash) {
-    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+var url = document.location.toString();
+console.log(url);
+if (url.match('#')) {
+    console.log("Hash present");
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
 } 
 
 // Change hash for page-reload
-$('.nav-tabs a').on('shown', function (e) {
-    window.location.hash = e.target.hash.replace("#", "#" + prefix);
-});
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
+
+$('.nav-tabs').stickyTabs();
